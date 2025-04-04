8128b9055257b448a63a72d45ee980f43b9a1304
mrachuta <34251394+mrachuta@users.noreply.github.com>
2025-04-04 14:41:44 +0200

    Add delay mechanism

--- src/main/java/com/devopstraining/spring/api/azure/passwordlessdbapp/TodoController.java
+++ src/main/java/com/devopstraining/spring/api/azure/passwordlessdbapp/TodoController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Value;
+import java.util.concurrent.ThreadLocalRandom;
 
 @RestController
 @RequestMapping("/api")
@@ -12,6 +13,9 @@ public class TodoController {
 
     @Value("${STYLE_VERSION:v1}")
     private String styleVersion;
+
+    @Value("${RANDOM_DELAYS:false}")
+    private Boolean randomDelays;
 
     private final TodoRepository todoRepository;
 
@@ -32,6 +36,16 @@ public class TodoController {
 
     @GetMapping("/config")
     public Map<String, Object> getConfigurationAsJson() {
+        if (randomDelays) {
+            int sleepTime = ThreadLocalRandom.current().nextInt(1000, 10001);
+            try {
+                System.out.println("Sleeping for " + sleepTime + " milliseconds...");
+                Thread.sleep(sleepTime);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                System.err.println("Sleep interrupted: " + e.getMessage());
+            }
+        }
         HashMap<String, Object> configMap = new HashMap<String, Object>();
         configMap.put("style_version", styleVersion);
         String tableStyle = (styleVersion.equals("v2")) ? "table-extra" : "table-standard";
